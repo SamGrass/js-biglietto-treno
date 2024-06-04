82ab643932b2ed3571788a0350551e2078923528
Samgrass <samuelegrassetti01@gmail.com>
2024-06-04 16:23:42 +0200

    add age discounts

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,23 @@
 //chiedere all’utente il numero di chilometri che vuole percorrere
-let userKm = parseFloat(prompt("inserisci il numero di chilometri da percorrere"))
+let userKm = parseFloat(prompt("inserisci il numero di chilometri da percorrere, segna i decimali con il 'punto'"));
 //l’età del passeggero.
-let userAge = parseInt(prompt("inserisci la tua età"))
+let userAge = parseInt(prompt("inserisci la tua età"));
 
 console.log(userKm, userAge);
 //calcolare il prezzo totale del viaggio
 
-//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//calcolare prezzo biglietto in base ai km (0.21 € al km)
+let pricePerKm = userKm * 0.21;
 
+console.log(pricePerKm);
 
+let totalPrice;
 
-//va applicato uno sconto del 20% per i minorenni
+// calcolare lo sconto minorenni
+let minorsDiscount = 80 / 100;
 
-//va applicato uno sconto del 40% per gli over 65.
+// calcolare lo sconto anziani
+let eldersDiscount = 60 / 100;
+
 
 // L’output del prezzo finale va messo (con massimo due decimali, per indicare centesimi sul prezzo).
